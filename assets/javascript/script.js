e12dd077c9a1b9ae4af390dca01df0c78306b6f4
Jonathan W <jwilliams12117@gmail.com>
2022-01-02 11:30:50 -0700

    Updated pseudocode to structure the javascript file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,18 @@
 // Define code to pass into the writePassword() function below
 
+//Prompts that will generate when the generateBtn is clicked
+var prompts = [];
+
+//Select criteria from the list to include in password
+
+//prompt the length of the password
+
+//prompt whether to include special chars, capitalization, numeric values
+
+//Run the password generation with randomly selected values from among given criteria
+
+//Display password in alert or write to page
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
