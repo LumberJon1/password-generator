3f0dd126d23ed004d156fe82e7cd70dfd4962d94
Jonathan W <jwilliams12117@gmail.com>
2022-01-02 08:33:24 -0700

    Cloned given code with acceptance criteria, linked files, organized structure

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+// Define code to pass into the writePassword() function below
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
