4442daee6f5961a80b9183eea56d734c4cc022e5
Jonathan W <jwilliams12117@gmail.com>
2022-01-02 21:42:34 -0700

    Added the prompt to the user to choose password criteria

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -78,16 +78,98 @@ var alphabetList = [
     "z"
 ]
 
+//Criteria list
+var criteriaList = [
+    "1. Special Characters",
+    "2. Numbers",
+    "3. Capital Letters"
+]
+
 //Run the password generation with randomly selected values from among given criteria
 var generatePassword = function() {
     
     //Select criteria from the lists to include in password
 
-        //Prompt with numbers
+    //Alert with the general task at hand
+    window.alert("Select criteria to include in your randomly generated password.");
 
-        //validate user input
+    /*Loop through the criteriaPrompt until the user has:
+    1. Selected all 3 inclusions for their password
+    2. Has selected "Done"
+    - Ensure user cannot enter unexpected values
+    */
 
-        //prompt again or exit loop
+    //Array that holds the criteria that the user selects in the prompt below
+    chosenPrompts = [];
+
+    //Prompt with numbers
+    //variables to correspond with cases and be displayed in the window prompt
+    var criteriaOne = "1. Special Characters";
+    var criteriaTwo = "2. Numbers";
+    var criteriaThree = "3. Capital Letters";
+
+    //Check that there are still criteria to choose from
+    while (criteriaOne != "" | criteriaTwo != "" | criteriaThree != "") {
+
+        var criteriaPrompt = window.prompt("Type the corresponding number for each of the following criteria you would like your password to include.\n"+
+        "Type \"Done\" when you are finished.\n\n"+criteriaOne+"\n"+criteriaTwo+"\n"+criteriaThree);
+
+        switch (criteriaPrompt) {
+
+            //Cases for special characters (be sure to capture typing the words)
+            case "1":
+            case "special characters":
+            case "Special Characters":
+
+                //Set the corresponding variable to a blank string.
+                criteriaOne = "";
+                //Add the user's choice to the array
+                chosenPrompts.push("1");
+
+                break;
+
+            //Cases for numerical values
+            case "2":
+            case "numbers":
+            case "Numbers":
+                criteriaTwo = "";
+                chosenPrompts.push("2");
+
+                break;
+
+            //Cases for capital letters
+            case "3":
+            case "capital letters":
+            case "Capital Letters":
+                criteriaThree = "";
+                chosenPrompts.push("3");
+
+                break;
+
+            //Cases to handle "Done"
+            case "Done":
+            case "done":
+                //Empty the strings so that the loop will close
+                criteriaOne = "";
+                criteriaTwo = "";
+                criteriaThree = "";
+
+                break;
+
+            //Default case
+            default:
+                window.alert("Invalid input. Enter from the given list of criteria to include.");
+
+        }
+    }
+
+    window.alert("Moving on to next section.")
+
+    //Use a switch to evaluate the input and call criteriaPrompt again if it is invalid.
+
+    //Remove the corresponding number from the prompt if it has already been chosen.
+
+
 
     //prompt the length of the password
 
